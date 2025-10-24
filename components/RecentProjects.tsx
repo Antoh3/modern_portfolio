aa0290cd73f6ad225194f79e674374037a7dac28
Antoh3 <emmanuelmuuo75@gmail.com>
2025-10-25 01:13:58 +0300

    Fix: add legacy-peer-deps for Vercel build

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,25 +13,18 @@ function RecentProjects() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center p-1 mt-5">
         {projects.map((item) => (
           <div
-            className="lg:min-h-[32.5rem] h-[25rem] mt-0 flex items-center justify-center sm:w-96 w-[90vw] gap-4"
+            className="lg:min-h-[32.5rem] h-[25rem] mt-0 flex items-center justify-center sm:w-80 w-[85vw] gap-4 mb-10 p-2"
             key={item.id}
           >
-            <PinContainer
-              title="View on Github"
-              href={item.link}
-            >
-              <div className="relative flex items-center justify-center sm:w-96 w-[60vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+            <PinContainer title="View on Github" href={item.link}>
+              <div className="relative flex items-center justify-center sm:w-80 w-[55vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
                   <img src="/bg.png" alt="bgimg" />
                 </div>
-                <img
-                  src={item.img}
-                  alt="cover"
-                  className="z-10 absolute bottom-0"
-                />
+                <img src={item.img} alt="cover" className="z-10 absolute bottom-0" />
               </div>
 
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
@@ -40,10 +33,7 @@ function RecentProjects() {
 
               <p
                 className="lg:text-xl lg:font-normal font-light text-sm"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
+                style={{ color: "#BEC1DD", margin: "1vh 0" }}
               >
                 {item.des}
               </p>
@@ -54,9 +44,7 @@ function RecentProjects() {
                     <div
                       key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
-                      }}
+                      style={{ transform: `translateX(-${5 * index + 2}px)` }}
                     >
                       <img src={icon} alt="icon5" className="p-2" />
                     </div>
@@ -74,6 +62,7 @@ function RecentProjects() {
           </div>
         ))}
       </div>
+
     </div>
   )
 }
